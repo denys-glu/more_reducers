861c0d1c58cfcf96307d4a47e0d8e4cefbdd0628
denys_gl <oggyla@gmail.com>
2021-01-08 12:42:49 -0800

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PostList from './PostList';
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+      <PostList />
+      </div>
+    </div>
+  );
+};
+
+export default App
